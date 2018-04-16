111c1d0e6ee0bdf1020fdc88e40494e2c1d19d26
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-04-16 20:38:13 +0530

    added utils

---
+++ src/com/cakeshop/dao/DbUtil.java
@@ -0,0 +1,5 @@
+package com.cakeshop.dao;
+
+public class DbUtil {
+
+}
---
+++ src/com/cakeshop/util/IdGenerator.java
@@ -0,0 +1,8 @@
+package com.cakeshop.util;
+
+public class IdGenerator {
+
+	public String getItemId() {
+		return "ITM_" + System.currentTimeMillis();
+	}
+}
