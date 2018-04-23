b077454097721ba6b58c732ace5858d44b7a293b
Charu24 <38050613+Charu24@users.noreply.github.com>
2018-04-24 00:53:14 +0530

    I have added CustomerAddress SQL file
    Please let me know if I have to write 'create table' code logic in DAO.

---
+++ SQL_commands.sql
@@ -0,0 +1,11 @@
+create table CUST_Address(
+FlatNo varchar(10),
+Street  varchar(30),
+Area varchar(30),
+Landmark varchar(30),
+PinCode number(8),
+City varchar(30),
+State varchar(30),
+Country varchar(30)
+);
+
