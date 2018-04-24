537ac1e4485a7b4985eb60a8c65425e17f93c933
Sangues <sangeethsudhakar94@gmail.com>
2018-04-25 00:46:09 +0530

    property file adding
    can you please tell its wrong or right

--- src/com/cakeshop/dao/DbUtil.java
+++ src/com/cakeshop/dao/DbUtil.java
@@ -3,6 +3,8 @@ package com.cakeshop.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class DbUtil {
 
@@ -11,7 +13,13 @@ public class DbUtil {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		if (con == null) {
 			Class.forName("org.h2.Driver");
-			return DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+			
+			ResourceBundle rb = ResourceBundle.getBundle("com//jm2006//learn//jdbc//util//db", Locale.US);
+			String url = rb.getString("jdbcUrl");
+			String un = rb.getString("userName");
+			String pass = rb.getString("password");
+
+			return DriverManager.getConnection(url, un, pass);
 		}
 		return con;
 	}
