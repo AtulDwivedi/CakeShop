2adf45b2dc01633904c803c50d1ef76351e2e379
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-04-28 14:02:20 +0530

    email over SSL

--- src/com/cakeshop/domain/Email.java
+++ src/com/cakeshop/domain/Email.java
@@ -2,6 +2,8 @@ package com.cakeshop.domain;
 
 public class Email {
 
+	private String fromAddress;
+	
 	private String toAddress;
 
 	private String ccAddress;
@@ -11,4 +13,58 @@ public class Email {
 	private String subject;
 
 	private String emailContent;
+
+	public String getToAddress() {
+		return toAddress;
+	}
+
+	public void setToAddress(String toAddress) {
+		this.toAddress = toAddress;
+	}
+
+	public String getCcAddress() {
+		return ccAddress;
+	}
+
+	public void setCcAddress(String ccAddress) {
+		this.ccAddress = ccAddress;
+	}
+
+	public String getBccAddress() {
+		return bccAddress;
+	}
+
+	public void setBccAddress(String bccAddress) {
+		this.bccAddress = bccAddress;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getEmailContent() {
+		return emailContent;
+	}
+
+	public void setEmailContent(String emailContent) {
+		this.emailContent = emailContent;
+	}
+
+	public Email() {
+		super();
+	}
+
+	public String getFromAddress() {
+		return fromAddress;
+	}
+
+	public void setFromAddress(String fromAddress) {
+		this.fromAddress = fromAddress;
+	}
+	
+	
 }
