20e5893b1ea18f18bdffecf43d503fbc2fe67274
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-04-28 14:18:08 +0530

    added address table

--- src/com/cakeshop/dao/DDL.sql
+++ src/com/cakeshop/dao/DDL.sql
@@ -13,3 +13,16 @@ CREATE TABLE cs_item
   	email		varchar,
   	password	varchar
   );
+  
+  CREATE TABLE cs_address
+(
+	Id varchar(50) PRIMARY KEY,
+	Fk varchar(50),
+	FlatNo varchar(10),
+	Area varchar(30),
+	Landmark varchar(30),
+	PinCode number(8),
+	City varchar(30),
+	State varchar(30),
+	Country varchar(30)
+);
