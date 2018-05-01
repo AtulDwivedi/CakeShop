8df1f269fb81b0f503426433c71c19fc4e446770
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-05-01 21:53:38 +0530

    added util methods for ID generations

--- src/com/cakeshop/util/IdGenerator.java
+++ src/com/cakeshop/util/IdGenerator.java
@@ -5,4 +5,16 @@ public class IdGenerator {
 	public static String getItemId() {
 		return "ITM_" + System.currentTimeMillis();
 	}
+
+	public static String getCustomerId() {
+		return "CST_" + System.currentTimeMillis();
+	}
+
+	public static String getAddressId() {
+		return "ADD_" + System.currentTimeMillis();
+	}
+
+	public static String getOrderId() {
+		return "ORD_" + System.currentTimeMillis();
+	}
 }
